890fd3428785835e678d02f4d49c79788491f355
Crxa <carakawidi07@gmail.com>
2024-06-03 18:37:22 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama : Caraka Widi Saputra";
+        echo "Kelas : 17.6B.4";
+        echo "NIM : 17210390";
+    }
+}
---
+++ application/views/v_footer.php
@@ -0,0 +1,6 @@
+<footer> <a href="http://crxanode.com">CrxaNodes</a>
+</footer>
+</div>
+</body>
+
+</html>
